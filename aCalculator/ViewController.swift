08c668fc8b7ae62ca620b0903e2cf0f9cceaad66
Fredrick Ohen <fredrickohen@gmail.com>
2017-03-25 15:38:30 -0500

    Initial commit

---
+++ aCalculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  aCalculator
+//
+//  Created by Fredrick Ohen on 3/25/17.
+//  Copyright © 2017 geeoku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+
+  @IBAction func touchDigit(_ sender: UIButton) {
+    
+    print("Digit button pressed")
+  }
+  
+
+}
+
